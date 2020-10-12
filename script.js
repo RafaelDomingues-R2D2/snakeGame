f7b8594e2b4e55d2671b3b9f327830ead724f768
Rafael <rafael.dominguesbr@gmail.com>
2020-10-11 22:22:47 -0300

    Criar a comida

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 //Cria o canvas
 function criarBG(){
@@ -26,11 +30,16 @@ function criarCobrinha(){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update (event){
     if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event,keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
@@ -43,6 +52,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
